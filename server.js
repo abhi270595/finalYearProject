9459e3cc7731002a0c6bd23c702b4bbf1c92149e
Abrar <mdabrarniyazi@gmail.com>
2016-04-06 00:19:11 +0530

    Enter Port Number in Command Line as an argument

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ var express = require("express");   // web framework external module
 var io      = require("socket.io"); // web socket external module
 var easyrtc = require("easyrtc");   // EasyRTC external module
 
+var portnum = process.argv[2];
+
 // Setup and configure Express http server. Expect a subfolder called "static" to be the web root.
 var httpApp = express();
 httpApp.use(express.static(__dirname + "/static/"));
@@ -15,7 +17,7 @@ var webServer = https.createServer(
 	key:  fs.readFileSync("./key.pem"),
 	cert: fs.readFileSync("./cert.pem")
 },
-httpApp).listen(8080);
+httpApp).listen(portnum);
 
 // Start Socket.io so it attaches itself to Express server
 var socketServer = io.listen(webServer, {"log level":1});
